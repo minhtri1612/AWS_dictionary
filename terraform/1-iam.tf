0ac52c5e4c50d37e0f63dea718ac37de3980d5e2
Minh Tri <10422119@student.vgu.edu.vn>
2025-08-20 18:23:58 +0700

    done amplify

---
+++ terraform/1-iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "amplify_service_role" {
+  name = "amplify-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "amplify.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "amplify_policy" {
+  role       = aws_iam_role.amplify_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
+}
---
+++ terraform/2-amplify.tf
@@ -0,0 +1,19 @@
+# Amplify app
+resource "aws_amplify_app" "my_app" {
+  name                = "AWS_dictionary"
+  repository          = "https://github.com/minhtri1612/AWS_dictionary.git" # your repo
+  iam_service_role_arn = aws_iam_role.amplify_service_role.arn
+
+  # Optional auto-branch
+  auto_branch_creation_config {
+    enable_auto_build = true
+  }
+}
+
+# Optional: Connect main branch
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.my_app.id
+  branch_name = "main"
+  framework   = "HTML"  # since you said only html
+  enable_auto_build = true
+}
