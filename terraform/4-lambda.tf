75036db2f700d216530bfa34bb85f294022c4729
minhtri1612 <10422119@vgu.edu.vn>
2025-09-23 15:09:32 +0700

    xóa search

--- terraform/4-lambda.tf
+++ terraform/4-lambda.tf
@@ -71,25 +71,4 @@ resource "aws_lambda_function" "populate_data" {
 }
 
 # Lambda function for adding new services
-resource "aws_lambda_function" "add_service" {
-  filename         = "../lambda/add_service.zip"
-  source_code_hash = filebase64sha256("../lambda/add_service.zip")
-  function_name    = "add-service"
-  role            = aws_iam_role.lambda_execution_role.arn
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  timeout         = 30
-  memory_size     = 128
-
-  environment {
-    variables = {
-      DYNAMODB_TABLE = aws_dynamodb_table.aws_services.name
-    }
-  }
-
-  tags = {
-    Name        = "add-service"
-    Environment = "production"
-    Project     = "aws-dictionary"
-  }
-}
+// add_service Lambda removed per user request
--- terraform/6-api-gateway.tf
+++ terraform/6-api-gateway.tf
@@ -28,12 +28,6 @@ resource "aws_api_gateway_resource" "search" {
   path_part   = "search"
 }
 
-# API Gateway Resource for services
-resource "aws_api_gateway_resource" "services" {
-  rest_api_id = aws_api_gateway_rest_api.dictionary_api.id
-  parent_id   = aws_api_gateway_rest_api.dictionary_api.root_resource_id
-  path_part   = "services"
-}
 
 # API Gateway Resource for populate
 resource "aws_api_gateway_resource" "populate" {
@@ -58,13 +52,6 @@ resource "aws_api_gateway_method" "search_services" {
   authorization = "NONE"
 }
 
-# POST method for adding services
-resource "aws_api_gateway_method" "add_service" {
-  rest_api_id   = aws_api_gateway_rest_api.dictionary_api.id
-  resource_id   = aws_api_gateway_resource.services.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
 
 # POST method for populate
 resource "aws_api_gateway_method" "populate_data" {
@@ -96,16 +83,6 @@ resource "aws_api_gateway_integration" "search_services_integration" {
   uri                    = aws_lambda_function.search_services.invoke_arn
 }
 
-# Integration for add service
-resource "aws_api_gateway_integration" "add_service_integration" {
-  rest_api_id = aws_api_gateway_rest_api.dictionary_api.id
-  resource_id = aws_api_gateway_resource.services.id
-  http_method = aws_api_gateway_method.add_service.http_method
-
-  integration_http_method = "POST"
-  type                   = "AWS_PROXY"
-  uri                    = aws_lambda_function.add_service.invoke_arn
-}
 
 # Integration for populate data
 resource "aws_api_gateway_integration" "populate_data_integration" {
@@ -136,14 +113,6 @@ resource "aws_lambda_permission" "search_services_permission" {
   source_arn    = "${aws_api_gateway_rest_api.dictionary_api.execution_arn}/*/*"
 }
 
-# Lambda permission for add service
-resource "aws_lambda_permission" "add_service_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.add_service.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.dictionary_api.execution_arn}/*/*"
-}
 
 # Lambda permission for populate data
 resource "aws_lambda_permission" "populate_data_permission" {
@@ -159,7 +128,6 @@ resource "aws_api_gateway_deployment" "dictionary_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.get_definition_integration,
     aws_api_gateway_integration.search_services_integration,
-    aws_api_gateway_integration.add_service_integration,
     aws_api_gateway_integration.populate_data_integration,
   ]
 
